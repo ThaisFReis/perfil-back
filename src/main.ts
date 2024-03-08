7e9bdc7156fb4cb00193eab54f944640cdd796ff
Thais Reis <thaisfreis@ufrj.br>
2024-03-08 20:58:24 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Definir as opções CORS
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3001', // Permitir solicitações apenas deste domínio
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Permitir credenciais de cookies ou cabeçalhos
   };
@@ -15,6 +15,6 @@ async function bootstrap() {
   // Habilitar o CORS com as opções definidas
   app.enableCors(corsOptions);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
